7d3f8cb602a3ca61d0956837cffc34cba8a73b6e
Archierowe <archierowe@gmail.com>
2020-08-15 11:42:41 +0000

    added countries data to JS

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,51 +1,53 @@
+var countries = {
+
+    'fr': {
+        center: { lat: 46.2276, lng: 2.2137 },
+        zoom: 5
+    },
+    'it': {
+        center: { lat: 41.8719, lng: 12.5674 },
+        zoom: 5
+    },
+    'es': {
+        center: { lat: 40.4637, lng: -3.7492 },
+        zoom: 5
+    },
+    'uk': {
+        center: { lat: 55.3781, lng: -3.4360 },
+        zoom: 5
+    }
+};
+
+
 function initMap() {
-            var map = new google.maps.Map(document.getElementById("map"), {
-                center: { lat: 51.5194, lng: 0.1270 },
-                zoom: 7
-            });
+//    I want to link this to the country selector - so when a user selects a country it snaps to that location on the map
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: countries ['uk'].center,
+        zoom: 3
+    });
 
-            var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
 
-            var locations = [
-            { lat: 51.4978, lng: 0.1745 },
-            { lat: 51.4967, lng: 0.1764 },
-            { lat: 51.5081, lng: 0.0759 }
-            ];
 
-            var markers = locations.map(function(location, i) {
-                return new google.maps.Marker({
-                    position: location,
-                    label: labels[i % labels.length]
-                });
-            });
+
+
+
+        //     var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
+
+        //     var locations = [
+        //     { lat: 51.4978, lng: 0.1745 },
+        //     { lat: 51.4967, lng: 0.1764 },
+        //     { lat: 51.5081, lng: 0.0759 }
+        //     ];
+
+        //     var markers = locations.map(function(location, i) {
+        //         return new google.maps.Marker({
+        //             position: location,
+        //             label: labels[i % labels.length]
+        //         });
+        //     });
         
 
-        var markerCluster = new MarkerClusterer(map, markers,
-            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+        // var markerCluster = new MarkerClusterer(map, markers,
+        //     {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
             
 }
-// found this on the google maps API site as I like the idea of markers dropping in - stuck it in here changed the long and lat.
-// var marker;
-
-// function initMap() {
-//   var map = new google.maps.Map(document.getElementById("map"), {
-//     zoom: 10,
-//     center: {lat: 51.5194, lng: 0.1270
-//   });
-
-//   marker = new google.maps.Marker({
-//     map: map,
-//     draggable: true,
-//     animation: google.maps.Animation.DROP,
-//     position: {lat: 51.5194, lng: 0.1270}
-//   });
-//   marker.addListener('click', toggleBounce);
-// }
-
-// function toggleBounce() {
-//   if (marker.getAnimation() !== null) {
-//     marker.setAnimation(null);
-//   } else {
-//     marker.setAnimation(google.maps.Animation.BOUNCE);
-//   }
-// }
